fbab2ca37d20bf936b84badf3686fa96c2225e23
yevin <yevin.dong@gmail.com>
2025-12-16 23:15:13 +0800

    feat: 更新聊天接口为GET请求并添加流式响应支持，新增前端index.html

--- src/service/main.py
+++ src/service/main.py
@@ -1,9 +1,19 @@
 import json
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from .model import Req, Resp
 from agent.graph import graph as workflow
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有来源
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许所有HTTP方法
+    allow_headers=["*"],  # 允许所有请求头
+)
 
 
 @app.get("/health")
@@ -11,16 +21,56 @@ def health():
     return {"status": "ok"}
 
 
-@app.post("/chat", response_model=Resp)
-def chat_endpoint(req: Req):
+@app.get("/chat", response_model=Resp)
+def chat_endpoint(
+    user_input: str = Query(..., description="用户输入"),
+    thread_id: str = Query(..., description="会话id")
+):
     config = {
         "configurable": {
-            "thread_id": req.thread_id,
+            "thread_id": thread_id,
             "enable_websearch": False,
             "enable_thinking": False
         }
     }
-    state_input = {"user_input": req.user_input}
+    state_input = {"user_input": user_input}
     resp = workflow.invoke(state_input, config)
-    print(resp)
     return Resp(reply=resp)
+
+
+@app.get("/stream")
+async def chat_endpoint(
+    user_input: str = Query(..., description="用户输入"),
+    thread_id: str = Query(..., description="会话id")
+):
+    config = {
+        "configurable": {
+            "thread_id": thread_id,
+            "enable_websearch": False,
+            "enable_thinking": False
+        }
+    }
+    state_input = {"user_input": user_input}
+    # TODO sse应该是订阅机制，但是这里就是模拟一下直接触发workflow了
+    # TODO stream 这里直接把所有snapshot状态暴漏了，其实不符合规范
+
+    async def sse_generator():
+        async for chunk in workflow.astream(state_input, config):
+            node_name, snapshot = next(iter(chunk.items()))
+            data = json.dumps({
+                "node_name": node_name,
+                "snapshot": snapshot
+            })
+            yield f"event: send\ndata: {data}\n\n"
+
+        # 发送结束事件
+        yield f"event: end\ndata: Stream finished\n\n"
+
+    return StreamingResponse(
+        sse_generator(),
+        media_type="text/event-stream",
+        headers={
+            "Cache-Control": "no-cache",
+            "X-Accel-Buffering": "no"
+        }
+    )
